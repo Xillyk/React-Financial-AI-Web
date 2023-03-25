a19c54ce9364f4811994ee58a8c136eb0799fa3d
Xillyk <xillyk@hotmail.com>
2023-03-25 19:32:32 +0700

    add api get transaction

--- src/state/api.ts
+++ src/state/api.ts
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { GetKpisResponse, GetProductResponse } from './types'
+import { GetKpisResponse, GetProductResponse, GetTransactionsResponse } from './types'
 
 // call endtpoint of backend
 export const api = createApi({
 	baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
 	reducerPath: "main",
-	tagTypes: ["Kpis", "Products"],
+	tagTypes: ["Kpis", "Products", "Transactions"],
 	endpoints: (build) =>({
 		// api call
 		getKpis: build.query<Array<GetKpisResponse>, void>({
@@ -16,8 +16,11 @@ export const api = createApi({
 			query: () => "product/products/",
 			providesTags: ["Products"]
 		}),
-
+		getTransactions: build.query<Array<GetTransactionsResponse>, void>({
+			query: () => "transaction/transactions/",
+			providesTags: ["Transactions"]
+		}),
 	})
 })
 
-export const { useGetKpisQuery, useGetProductsQuery } = api
+export const { useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery } = api
