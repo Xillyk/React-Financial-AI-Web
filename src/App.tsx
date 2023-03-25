a8dd44bb63a22bfdf8aca29c238be2262eb47b70
Xillyk <xillyk@hotmail.com>
2023-03-25 12:59:36 +0700

    fix classname error

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
-    <div className="App">
+    <div className="app">
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
